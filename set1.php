22b072a2d2494d7f1d474cbd8802c5aabfd575c4
igeh69 <115367952+igeh69@users.noreply.github.com>
2022-10-09 00:30:26 +0700

    Create set1.php

---
+++ set1.php
@@ -0,0 +1,20 @@
+<?php
+///////////////////////////////////////////////////////
+///////// KAGAK USAH DELET CREDIT AJG /////////////////
+///////////////////////////////////////////////////////
+
+# Creator: haha
+# youtube: haha
+
+
+# Masukan username akun ori/asli
+# Enter the original account username
+$youser="ccriyadi";
+
+# Username akun tumbal
+# Enter the username of the account that was sacrificed
+$t_user="putri.nadiajati";
+
+# Passwoard/sandi akun tumbal
+# enter the password of the account that was sacrificed
+$t_pass="@Janda69";
